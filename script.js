ed6afdbb48caed88233843fddf628dec75f44417
Adhil Khan <adhilkhan2023@gmail.com>
2024-05-05 17:11:46 +0530

    Create a function named operate

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ let firstNumber;
 let operator;
 
 let secondNumber;
+
+function operate(operator,firstNumber,secondNumber){
+    switch (operator) {
+        case "+":
+            return add(firstNumber,secondNumber);
+        case "-":
+            return subtract(firstNumber,secondNumber);
+        case "*":
+            return multiply(firstNumber,secondNumber);
+        case "/":
+            return divide(firstNumber,secondNumber);
+    }
+}
