23fbf34950bffffe217255da528c69a3e5f24b08
Adhil Khan <adhilkhan2023@gmail.com>
2024-05-06 17:27:27 +0530

    Remove evaluating more than single pair of numbers
    Remove the ability of calculator to evaluate more than single pair of
    numbers at a time.

--- script.js
+++ script.js
@@ -50,10 +50,18 @@ const operatorButtons = document.querySelectorAll('.operator-buttons');
 
 for(i = 0; i < operatorButtons.length; i++){
     operatorButtons[i].addEventListener('click',(event) => {
-        firstNumber = parseInt(displayValue);
-        display.textContent = '';
-        displayValue = '';
-        operator = event.target.textContent;
+        if(firstNumber){
+            secondNumber = parseInt(displayValue);
+            display.textContent = operate(operator,firstNumber,secondNumber);
+            displayValue = display.textContent;
+            firstNumber = '';
+            secondNumber = '';
+        }else{
+            firstNumber = parseInt(displayValue);
+            display.textContent = '';
+            displayValue = '';
+            operator = event.target.textContent;
+        }
     })
 }
 
@@ -63,6 +71,7 @@ equalSign.addEventListener('click',() => {
     secondNumber = parseInt(displayValue);
     display.textContent = operate(operator,firstNumber,secondNumber);
     displayValue = display.textContent;
+    firstNumber = '';
 })
 
 const resetButton = document.querySelector('.reset-button');
